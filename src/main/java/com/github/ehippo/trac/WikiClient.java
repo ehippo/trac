52fc899347b3ce03aeea8f5e3b727214f2ffd730
Alex Lototsky <alototsky@slice.com>
2015-04-24 22:42:50 +0300

    Finished wiki client

--- src/main/java/com/github/ehippo/trac/WikiClient.java
+++ src/main/java/com/github/ehippo/trac/WikiClient.java
@@ -118,4 +118,84 @@ public interface WikiClient {
      */
     Boolean putPage(String pagename, String content, HashMap<String,String> attributes);
 
+    /**
+     * Lists attachments on a given page.
+     *
+     * Permission required: By resource
+     */
+    List<String> listAttachments(String pagename);
+
+    /**
+     * returns the content of an attachment.
+     *
+     * Permission required: By resource
+     */
+    byte[] getAttachment(String path);
+
+    /**
+     * (over)writes an attachment. Returns True if successful. This method is
+     * compatible with WikiRPC. putAttachmentEx has a more extensive set of
+     * (Trac-specific) features.
+     * 
+     * Permission required: By resource
+     */
+    Boolean putAttachment(String path, byte[] data);
+
+    /**
+     * Attach a file to a Wiki page. Returns the (possibly transformed) filename
+     * of the attachment. Use this method if you don't care about WikiRPC
+     * compatibility.
+     * 
+     * Permission required: By resource
+     */
+    Boolean putAttachmentEx(String pagename, String filename, String description, byte[] data);
+
+    /**
+     * Attach a file to a Wiki page. Returns the (possibly transformed) filename
+     * of the attachment. Use this method if you don't care about WikiRPC
+     * compatibility.
+     * 
+     * Permission required: By resource
+     */
+    Boolean putAttachmentEx(String pagename, String filename, String description, byte[] data, Boolean replace);
+
+    /**
+     * Delete a Wiki page (all versions) or a specific version by including an
+     * optional version number. Attachments will also be deleted if page no
+     * longer exists. Returns True for success.
+     * 
+     * Permission required: By resource
+     */
+    Boolean deletePage(String name);
+
+    /**
+     * Delete a Wiki page (all versions) or a specific version by including an
+     * optional version number. Attachments will also be deleted if page no
+     * longer exists. Returns True for success.
+     * 
+     * Permission required: By resource
+     */
+    Boolean deletePage(String name, Integer version);
+
+    /**
+     * Delete an attachment.
+     * 
+     * Permission required: By resource
+     */
+    Boolean deleteAttachment(String path);
+
+    /**
+     * Not implemented
+     * 
+     * Permission required: WIKI_VIEW
+     */
+    List<String> listLinks(String pagename);
+
+    /**
+     * Render arbitrary Wiki text as HTML.
+     * 
+     * Permission required: WIKI_VIEW
+     */
+    String wikiToHtml(String text);
+
 }
