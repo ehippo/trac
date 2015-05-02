381ff1e099e43c93374c252e99bbcd60dac36ec2
Alexander Lototsky <alototsky@slice.com>
2015-05-02 19:20:03 +0300

    Just LogEntry class

---
+++ src/test/java/com/github/ehippo/trac/TicketClientExTest.java
@@ -0,0 +1,15 @@
+package com.github.ehippo.trac;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TicketClientExTest {
+
+    @Test
+    public void testGet() {
+        TicketClientEx tcex = new TicketClientEx(null);
+        assertEquals(1, 1);
+    }
+
+}
